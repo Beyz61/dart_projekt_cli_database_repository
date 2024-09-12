b73973750b7e549880fe9daa8d7b4e39b1a9ccca
beyzanur <beyzanur.o@icloud.com>
2024-09-12 15:53:03 +0200

    Mockdatabase Angelegt

---
+++ data/mock_database.dart
@@ -0,0 +1,47 @@
+//Aufgabe 7
+//Aufgabe 7
+import '../models/recipe.dart';
+import '../models/user.dart';
+import 'database_repository.dart';
+
+class MockDatabase implements DatabaseRepository {
+  List<User> users = [];
+  List<Recipe> recipes = [
+    Recipe(
+        title: "Käsekuchen",
+        ingredients: ["Eier", "Quark"],
+        preparation: "Misch alles.")
+  ];
+// Warum kann ich das in der main nicht abrufen?
+  @override
+  void addRecipe(Recipe newRecipe) {
+    recipes.add(newRecipe);
+  }
+
+  @override
+  void addUser(String userName, String password, String email) {}
+
+  @override
+  bool checkUserCredentials(String userName, String password, String email) {
+    throw UnimplementedError();
+  }
+
+  @override
+  void editUser(User user) {}
+
+  @override
+  List<Recipe> getAllRecipes() {
+    return recipes;
+  }
+
+  @override
+  void removeRecipe(Recipe recipe) {
+    recipes.remove(recipe);
+  }
+
+  @override
+  void removeUser(String recipeId) {}
+
+  @override
+  void updateRecipe(String recipeId, Recipe updatedRecipe) {}
+}
